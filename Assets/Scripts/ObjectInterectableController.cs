9f28800cd4ccc4175c3a434e8a3c165f70d02f1b
DESKTOP-I8IRS9D\Bintang Dwitama <bintangdwitama160501@gmail.com>
2021-11-17 23:35:36 +0700

    interactable object v.1

---
+++ Assets/Scripts/ObjectInterectableController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInterectableController : MonoBehaviour
+{
+
+    public bool interacted;
+    private float xPosition;
+
+    void Start()
+    {
+        xPosition = transform.position.x;
+    }
+
+    void Update()
+    {
+        if (interacted == false)
+        {
+            transform.position = new Vector2(xPosition, transform.position.y);
+        } else
+        {
+            xPosition = transform.position.x;
+        }
+    }
+}
